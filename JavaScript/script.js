892a3c00986656358ac82bd33135dab050156fe1
Madaminsher19 <madaminsher19@gmail.com>
2023-07-21 22:43:08 +0500

    homme work end

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -7,27 +7,27 @@ console.log(natija);
 // 2-masala
 var c = 25;
 var d = 15;
-var result=c-d;
+var result = c - d;
 console.log(result);
 
 // 3-masala
-var e=10;
-var f=5;
-var j=e-f;
-var k=7;
-var result1= j*k;
+var e = 10;
+var f = 5;
+var j = e - f;
+var k = 7;
+var result1 = j * k;
 console.log(result1);
 
 // 4-masala
 
-var m=10;
-var n=3;
-var result2 = m%n;
+var m = 10;
+var n = 3;
+var result2 = m % n;
 console.log(result2);
 
 // 5-masala
 
-var x=2;
-var y=10; 
-var result3=x**y;
+var x = 2;
+var y = 10;
+var result3 = x ** y;
 console.log(result3);
