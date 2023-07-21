952c11ef7141bc3283a97d56729b013a4b35b5ba
Madaminsher19 <madaminsher19@gmail.com>
2023-07-21 22:35:54 +0500

    homme work

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,33 @@
-console.log("salom dunyo")
+// 1-masala
+var a = 10;
+var b = 5;
+var natija = a + b;
+console.log(natija);
+
+// 2-masala
+var c = 25;
+var d = 15;
+var result=c-d;
+console.log(result);
+
+// 3-masala
+var e=10;
+var f=5;
+var j=e-f;
+var k=7;
+var result1= j*k;
+console.log(result1);
+
+// 4-masala
+
+var m=10;
+var n=3;
+var result2 = m%n;
+console.log(result2);
+
+// 5-masala
+
+var x=2;
+var y=10; 
+var result3=x**y;
+console.log(result3);
