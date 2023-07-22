cc369874cf6cefd5c84df650da2141adb79e3f4b
Madaminsher19 <madaminsher19@gmail.com>
2023-07-22 19:00:28 +0500

    new post

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,33 +1,8 @@
-// 1-masala
-var a = 10;
-var b = 5;
-var natija = a + b;
-console.log(natija);
-
-// 2-masala
-var c = 25;
-var d = 15;
-var result = c - d;
-console.log(result);
-
-// 3-masala
-var e = 10;
-var f = 5;
-var j = e - f;
-var k = 7;
-var result1 = j * k;
-console.log(result1);
-
-// 4-masala
-
-var m = 10;
-var n = 3;
-var result2 = m % n;
-console.log(result2);
-
-// 5-masala
-
-var x = 2;
-var y = 10;
-var result3 = x ** y;
-console.log(result3);
+var a = 500;
+var b = 250;
+var c = a + b;
+var d = 120;
+var e = c * 10650.34;
+var f = d * 10650.03;
+var result = f + e;
+console.log(result)
