551719018fb9dcacbf448642da4b61d22af38d9e
Madaminsher19 <madaminsher19@gmail.com>
2023-07-25 13:00:41 +0500

    homme work

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,8 +1,25 @@
-var a = 500;
-var b = 250;
-var c = a + b;
-var d = 120;
-var e = c * 10650.34;
-var f = d * 10650.03;
-var result = f + e;
-console.log(result)
+var ball = prompt("balingizni kiriting");
+
+if (ball < 80) {
+    alert("o'qishga kira olmadingiz");
+} else if (ball < 100) {
+    alert("siz super-kontrakt asosida o'qishga tavsiya qilindingiz");
+    var pull = prompt("yillik kantrakt miqdori 3000$. Sizni pulingiz bor?");
+    if (pull < 3000) {
+        alert("Siz kontrakt miqdorini to'lay olmaysiz.");
+    } else if (pull > 3000) {
+        alert("tabriklayman siz bizda o'qishingiz mumkin");
+    }
+} else if (ball < 130) {
+    alert("siz kontrakt asosida o'qishga tavsiya qilindingiz");
+    var pull = prompt("yillik kantrakt miqdori 2000$. Sizni pulingiz bor?");
+    if (pull < 2000) {
+        alert("Siz kontrakt miqdorini to'lay olmaysiz.");
+    } else if (pull > 2000) {
+        alert("tabriklayman siz bizda o'qishingiz mumkin");
+    }
+} else if (ball <= 180) {
+    alert("Grant asosida o'qishga qabul qilindiz.");
+} else if (ball > 180) {
+    alert("siz no'tog'ri ball kiritdingiz");
+}
