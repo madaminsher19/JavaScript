c45030dd00e9e3eace1e9fbb831ae692b1f04a35
Madaminsher19 <madaminsher19@gmail.com>
2023-07-27 21:04:29 +0500

    new

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,15 +1,11 @@
-alert("Assalomu aleykum va roxmatullohi va barokatihu");
+var num = Number(prompt("raqam kiriting"));
 
-let nameA = prompt("ismingizni kiriting.");
-let money = prompt(nameA + " sizda qancha pull bor?");
-let a = 750;
-let b = 120;
-let dollar = a * 11000.34;
-let euro = b * 12354.03;
-let som = dollar + euro;
-let keraklisom = som - money 
-if(som > money){
-    console.log(nameA + " sizda " + keraklisom + " so'm mablag' yetarli emas");
-} else if(som <= money){
-console.log("Oq yo'l " + nameA);
+if (num>0 && num <10){
+    for(let i=1; i<11; i++){
+        console.log(`${num} * ${i} = ${num*i}`);
+    }
+} else if(num>10){
+    console.log("siz noto'gri raqam kiritdiz");
 }
+
+
