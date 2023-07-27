2eca8a651fe3e650d1eb2aad919e93ce1b02f812
Madaminsher19 <madaminsher19@gmail.com>
2023-07-27 07:47:10 +0500

    new home work

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,15 @@
-alert("Assalomu aleykum va roxmatullohi va barokatihu")
+alert("Assalomu aleykum va roxmatullohi va barokatihu");
+
+let nameA = prompt("ismingizni kiriting.");
+let money = prompt(nameA + " sizda qancha pull bor?");
+let a = 750;
+let b = 120;
+let dollar = a * 11000.34;
+let euro = b * 12354.03;
+let som = dollar + euro;
+let keraklisom = som - money 
+if(som > money){
+    console.log(nameA + " sizda " + keraklisom + " so'm mablag' yetarli emas");
+} else if(som <= money){
+console.log("Oq yo'l " + nameA);
+}
