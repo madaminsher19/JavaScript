bdd7eb0f661f6801e9b6b01b5c73229d976e123c
Madaminsher19 <madaminsher19@gmail.com>
2023-07-28 23:31:52 +0500

    new host

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,14 @@
-var num = Number(prompt("raqam kiriting"));
-
-if (num>0 && num <10){
-    for(let i=1; i<11; i++){
-        console.log(`${num} * ${i} = ${num*i}`);
+let num = Number(prompt("son kiriting"));
+let b;
+let c;
+for (let i = 0; i <= num; i++) {
+    if (num % i == 0) {
+        var d = b;
     }
-} else if(num>10){
-    console.log("siz noto'gri raqam kiritdiz");
+    break;
 }
-
-
+if (d) {
+    console.log("tub emas");
+} else if (c) {
+    console.log("tub son");
+}
