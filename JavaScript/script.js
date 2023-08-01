36337b1ec47465de43ceee70e6085dc7551ace1e
Madaminsher19 <madaminsher19@gmail.com>
2023-08-01 18:58:14 +0500

    tezlik1

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,32 +1,18 @@
-const  elinput= document.querySelector(".section__input");
-const  elbtn= document.querySelector(".section__btn");
-const  elpiyoda= document.querySelector("#piyoda");
-const  elvelosipedist= document.querySelector("#velosipedist");
-const  eltaxi= document.querySelector("#taxi");
-const  elsamalyot= document.querySelector("#samalyot");
+const elinput = document.querySelector(".section__input");
+const elbtn = document.querySelector(".section__btn");
+const elpiyoda = document.querySelector("#piyoda");
+const elvelosipedist = document.querySelector("#velosipedist");
+const eltaxi = document.querySelector("#taxi");
+const elsamalyot = document.querySelector("#samalyot");
 
-
 elbtn.addEventListener("click", function (evt) {
-  evt.preventDefault()
- elpiyoda.innerHTML=`
- ${elinput.value / 3.6 } soat ` 
-  
-})
-elbtn.addEventListener("click", function (evt) {
-  evt.preventDefault()
-  elvelosipedist.innerHTML=`
- ${elinput.value / 20.1 } soat ` 
-  
-})
-elbtn.addEventListener("click", function (evt) {
-  evt.preventDefault()
- eltaxi.innerHTML=`
- ${elinput.value / 70} soat ` 
-  
-})
-elbtn.addEventListener("click", function (evt) {
-  evt.preventDefault()
- elsamalyot.innerHTML=`
- ${elinput.value / 800 } soat ` 
-  
-})
+    evt.preventDefault();
+    elpiyoda.innerHTML = `
+ ${elinput.value / 3.6} soat `;
+    elvelosipedist.innerHTML = `
+ ${elinput.value / 20.1} soat `;
+    eltaxi.innerHTML = `
+ ${elinput.value / 70} soat `;
+    elsamalyot.innerHTML = `
+ ${elinput.value / 800} soat `;
+});
