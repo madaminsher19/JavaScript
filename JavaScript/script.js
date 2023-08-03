fefa8c6edad1d88878c0804ef3ee110aa113ee03
Madaminsher19 <madaminsher19@gmail.com>
2023-08-03 16:09:45 +0500

    new post2

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -9,7 +9,7 @@ elBtn.addEventListener("click", function (evt) {
     if (elInput.value < 11) {
         elBorder1.style.display = "block";
         elBorder2.style.display = "none";
-    }else if( elInput.value<16 && elInput.value >= 11){
+    }else if( elInput.value<=16 && elInput.value >= 11){
         elBorder1.style.display = "none";
         elBorder2.style.display = "block";
     }else if( elInput.value>16 ){
